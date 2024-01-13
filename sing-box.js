3b51c42a811767fe230e217f17417484fdcb7935
汐殇 <95160953+xishang0128@users.noreply.github.com>
2024-01-14 03:30:35 +0800

    Create sing-box.js

---
+++ sing-box.js
@@ -0,0 +1,59 @@
+const { type, name } = $arguments
+if (type == "1") {
+  subtype = 'collection'
+} else {
+  subtype = 'subscription'
+}
+
+let config = JSON.parse($files[0])
+let proxies = JSON.parse(await produceArtifact({
+  type: subtype,
+  name: name,
+  platform: 'sing-box'
+}))
+
+proxies.map(p => {
+  config.outbounds.push(p)
+  config.outbounds.map(i => {
+    if (['all', 'all-auto'].includes(i.tag)) {
+      i.outbounds.push(p.tag)
+    }
+  })
+})
+proxies.filter(p => /港|hk|hongkong|kong kong|🇭🇰/i.test(p.tag)).map(p => {
+  config.outbounds.map(i => {
+    if (['hk', 'hk-auto'].includes(i.tag)) {
+      i.outbounds.push(p.tag)
+    }
+  })
+})
+proxies.filter(p => /台|tw|taiwan|🇹🇼/i.test(p.tag)).map(p => {
+  config.outbounds.map(i => {
+    if (['tw', 'tw-auto'].includes(i.tag)) {
+      i.outbounds.push(p.tag)
+    }
+  })
+})
+proxies.filter(p => /日本|jp|japan|🇯🇵/i.test(p.tag)).map(p => {
+  config.outbounds.map(i => {
+    if (['jp', 'jp-auto'].includes(i.tag)) {
+      i.outbounds.push(p.tag)
+    }
+  })
+})
+proxies.filter(p => /^(?!.*(?:us)).*(新|sg|singapore|🇸🇬)/i.test(p.tag)).map(p => {
+  config.outbounds.map(i => {
+    if (['sg', 'sg-auto'].includes(i.tag)) {
+      i.outbounds.push(p.tag)
+    }
+  })
+})
+proxies.filter(p => /美|us|unitedstates|united states|🇺🇸/i.test(p.tag)).map(p => {
+  config.outbounds.map(i => {
+    if (['us', 'us-auto'].includes(i.tag)) {
+      i.outbounds.push(p.tag)
+    }
+  })
+})
+
+$content = JSON.stringify(config, null, 2)
